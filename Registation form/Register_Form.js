dbb50bb2d4fd27e0a1ce0a50ef9601cd094b4b33
vkpvivek <44742615+vkpvivek@users.noreply.github.com>
2023-06-18 11:34:19 +0530

    Add files via upload

---
+++ Registation form/Register_Form.js
@@ -0,0 +1,8 @@
+function submitForm(event){
+    console.log("Data")
+    console.log(document.getElementById('fname').value)
+    console.log(document.getElementById('email').value)
+    console.log(document.getElementById('phone').value)
+    console.log(document.getElementById('date').value)
+    console.log(document.getElementById('time').value)
+}
