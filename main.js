9abada50d2cc5130ae8d3b5ddc32ef324fa75632
vkpvivek <vkpvivek67@gmail.com>
2023-06-21 10:40:59 +0530

    commit FIlter add

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 var Form=document.getElementById('addForm');
 var ItemList=document.getElementById('items');
+var filter = document.getElementById('filter');
 
 //form submit event
 Form.addEventListener('submit',addItem);
 
 // Delete event
 ItemList.addEventListener('click', removeItem);
+
+// Filter event
+filter.addEventListener('keyup', filterItems);
+
+
 
 //...................Add new item................................
 function addItem(e){
@@ -44,4 +50,22 @@ function removeItem(e){
         ItemList.removeChild(li);
       }
     }
+  }
+
+
+//.......................Filter Items.............................
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get lis
+    var items = ItemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
   }
